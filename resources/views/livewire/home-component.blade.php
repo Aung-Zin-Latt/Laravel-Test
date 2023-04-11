e765dc1f2c0f53395faf6ae8554a1fbf366021a6
Aung-Zin-Latt <aungzinlatt007@gmail.com>
2023-04-11 14:46:08 +0630

    Add Button in HomeComponent

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <h1 class="main-content">Main Page</h1>
+    <div class="row">
+        <div class="col-md-6 mx-auto text-center">
+            <h1 class="main-content">Main Page</h1>
+            <a class="btn btn-primary my-5" href="{{ route('user.addform') }}">Add Custom Form</a>
+        </div>
+    </div>
 </div>
